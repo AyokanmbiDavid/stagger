dcc7861a9165679e18ae885e2adc4a76fdd4f134
AyokanmbiDavid <davidayokanmbi47@gmail.com>
2025-12-31 20:00:28 +0100

    stagger

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import { Navigate, Outlet, replace } from 'react-router-dom'
 
 const ProtectedRoute = () => {
-  const isLogged = JSON.parse(localStorage.getItem('staggerLog')) || [];
+  const isLogged = true
+  //  JSON.parse(localStorage.getItem('staggerLog')) || [];
 
-  if (!isLogged.email && !isLogged.username) {
+  if (!isLogged) {
     return <Navigate to={'/'} replace/>;
   }
 
